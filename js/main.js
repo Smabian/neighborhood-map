e78c9d5601a278fc791e06f8a17a8a7916b68681
Martin Wenzel <martin.wenzel@outlook.com>
2016-07-09 23:35:15 +0200

    Animation to markers added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var Locations = [
-  {title: 'O´Donovan`s Irish Pub', location: {lat: 48.6847732, lng: 9.0126629}},
-  {title: 'Movie Theater', location: {lat: 48.6840791, lng: 9.0119977}},
-  {title: 'Mercaden Store', location: {lat: 48.6871319, lng: 9.0060003}},
-  {title: 'Hewlett Packard Enterprise', location: {lat: 48.6840538, lng: 8.9693227}},
-  {title: 'Lidl Store', location: {lat: 48.6782971, lng: 9.0117677}},
-  {title: 'Train Station', location: {lat: 48.688063, lng: 9.0043825}}
+  {title: 'O´Donovan`s Irish Pub', location: {lat: 48.684770, lng: 9.014852}},
+  {title: 'Movie Theater', location: {lat: 48.683887, lng: 9.013464}},
+  {title: 'Mercaden Store', location: {lat: 48.687687, lng: 9.006613}},
+  {title: 'Hewlett Packard Enterprise', location: {lat: 48.6758462, lng: 8.9765623}},
+  {title: 'Lidl Store', location: {lat: 48.67854, lng: 9.0125401}},
+  {title: 'Train Station', location: {lat: 48.687601, lng: 9.0047014}}
 ]
 
 var Place = function(data){
@@ -18,27 +18,23 @@ var ViewModel = function(){
   this.locationList = ko.observableArray([]);
   this.query = ko.observable('');
 
-  // Initial Load of list
-  Locations.forEach(function(locationItem){
-    self.locationList.push(new Place(locationItem));
-  });
-
   // search function to update list
   self.search = function(value){
     // Clear List
     self.locationList.removeAll();
+    //clearMarkers(null);
     // run through locations and check if it matches the value entered in the search
     for(i=0;i<Locations.length;i++){
       if (Locations[i].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
         self.locationList.push(new Place(Locations[i]));
+       // setMarker(Locations[i]);
       }
     }
   }
 }
-
 ko.applyBindings(new ViewModel());
 
-
+//Google Map handling
 var map;
 var markers = [];
 
@@ -48,8 +44,9 @@ function initMap() {
           zoom: 15,
         });
 
+  var largeInfowindow = new google.maps.InfoWindow();
+
   for (var i = 0; i < Locations.length; i++) {
-    // Get the position from the location array.
     var position = Locations[i].location;
     var title = Locations[i].title;
     // Create a marker per location, and put into markers array.
@@ -60,7 +57,14 @@ function initMap() {
       id: i
     });
     markers.push(marker);
-    // Two event listeners - one for mouseover, one for mouseout
+    //Add Info Window
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+    marker.addListener('click', function() {
+      Bounce(this);
+    });
+    //Add Marker and scale map
     showListings();
   }
 }
@@ -74,3 +78,29 @@ function showListings() {
   }
   map.fitBounds(bounds);
 }
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    // Clear the infowindow content to give the streetview time to load.
+    infowindow.setContent('');
+    infowindow.marker = marker;
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    // Open the infowindow on the correct marker.
+    infowindow.open(map, marker);
+  }
+}
+
+function Bounce(marker) {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function(){ marker.setAnimation(null); }, 1400);
+}
+
+
